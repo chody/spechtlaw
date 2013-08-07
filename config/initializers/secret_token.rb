f7ed9cb3b3e1b2c190f25f13630cc8b7d4ffcadd
Cody Hill <cody.hill07@gmail.com>
2013-08-06 23:33:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spechtlaw::Application.config.secret_key_base = '135775af50803a197ceb9aa0a8b4591c95551bcf5123fb6cd96daa7b8f0299c9ea4a48e76784fd8e09d3c10263d4951777dcc554c8c3d2a79cbe85f67978fe7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spechtlaw::Application.config.session_store :cookie_store, key: '_spechtlaw_session'
